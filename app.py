4c7e78f3a2f0bd479dda4a5910d86c29f646a3b7
Arthur Nguekeu <88515280+nguekeuarthur@users.noreply.github.com>
2024-06-28 11:07:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-Dashboard_page = st.Page("Dashboard.py", title="Tableau de bord de l'Application", icon=":material/dashboard:")
-Usage_page = st.Page("Usage.py", title="Usage", icon="📈")
-Engagement_page = st.Page("Engagement.py", title="Engagement", icon="📲")
-Maps_page = st.Page("Maps.py", title="Maps", icon="🌐")
+Dashboard_page = st.Page("Dashboard.py", title="Tableau de bord ", icon=":material/dashboard:")
+Usage_page = st.Page("Usage.py", title="Audience", icon="📈")
+Engagement_page = st.Page("Engagement.py", title="Données analytiques", icon="📲")
+Maps_page = st.Page("Maps.py", title="Données géographique ", icon="🌐")
 
 pg = st.navigation([Dashboard_page, Usage_page,Engagement_page,Maps_page])
 st.set_page_config(page_title="Data manager", page_icon=":bar_chart:")
